f93a5ee3ffe85ae41ca286c59449424adca4ca39
tmori <kanetugu2015@gmail.com>
2025-06-11 14:05:40 +0900

    add websocket test

--- Plugins/HakoniwaPdu/Source/HakoniwaPdu/HakoniwaPdu.Build.cs
+++ Plugins/HakoniwaPdu/Source/HakoniwaPdu/HakoniwaPdu.Build.cs
@@ -25,7 +25,7 @@ public class HakoniwaPdu : ModuleRules
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core",
+				"Core", "WebSockets"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
--- Plugins/HakoniwaPdu/Source/HakoniwaPdu/Private/HakoniwaPdu.cpp
+++ Plugins/HakoniwaPdu/Source/HakoniwaPdu/Private/HakoniwaPdu.cpp
@@ -3,6 +3,37 @@
 #include "HakoniwaPdu.h"
 #include "Logging/LogMacros.h"
 
+#if 1
+#include "IWebSocket.h"
+#include "WebSocketsModule.h"
+
+TSharedPtr<IWebSocket> WebSocket;
+
+
+void ConnectWebSocket()
+{
+    if (!FModuleManager::Get().IsModuleLoaded("WebSockets"))
+    {
+        FModuleManager::Get().LoadModule("WebSockets");
+    }
+
+    FString Url = TEXT("ws://172.31.9.252:8765");  // © “K‹X•ÏX
+    WebSocket = FWebSocketsModule::Get().CreateWebSocket(Url);
+
+    WebSocket->OnConnected().AddLambda([]()
+        {
+            UE_LOG(LogTemp, Log, TEXT("WebSocket Connected!"));
+        });
+
+    WebSocket->OnConnectionError().AddLambda([](const FString& Error)
+        {
+            UE_LOG(LogTemp, Error, TEXT("WebSocket Connection Error: %s"), *Error);
+        });
+
+    WebSocket->Connect();
+}
+#endif
+
 #define LOCTEXT_NAMESPACE "FHakoniwaPduModule"
 DEFINE_LOG_CATEGORY_STATIC(LogHakoniwaPdu, Log, All);
 
@@ -10,6 +41,7 @@ void FHakoniwaPduModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 	UE_LOG(LogHakoniwaPdu, Log, TEXT("HakoniwaPdu: StartupModule called"));
+    ConnectWebSocket();
 }
 
 void FHakoniwaPduModule::ShutdownModule()
